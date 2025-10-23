4bd909d106956237d0569b4bb5947bd6c06c06a4
David Tang <david_tang@web.de>
2025-10-23 19:51:06 +0200

    ENZYME Added

---
+++ src/components/check_box_with_label.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+export default class CheckboxWithLabel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isChecked: false};
+  }
+  onChange = () => {
+    this.setState(prevState => ({ isChecked: !prevState.isChecked }));
+  }
+  render() {
+    return (
+      <label>
+        <input type="checkbox" checked={this.state.isChecked}
+          onChange={this.onChange} />
+        {this.state.isChecked ? this.props.labelOn : this.props.labelOff}
+      </label>
+    );
+  }
+}
